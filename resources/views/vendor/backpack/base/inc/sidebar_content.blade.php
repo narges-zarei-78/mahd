ec06adde37ffba4113c8b974c6b406e2e62e86e6
Your Name <you@example.com>
2023-09-07 23:41:25 +0430

    show menu for admins and teachers

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,6 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-users'></i> کاربران</a></li>
+@if(backpack_user()->hasRole('admin') || backpack_user()->hasRole('teacher'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-users'></i> کاربران</a></li>
+@endif
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('lesson') }}'><i class='nav-icon la la-book'></i> دروس</a></li>
